97b2e40cd8ce6ec08d99bfbce470781e3650a9a3
Anthony Passos dos Santos <anthony.passos@dinfor.uniceub>
2025-06-13 17:58:34 -0300

    arrumando tela de login

--- app/src/main/java/com/mera/babycare/Login.kt
+++ app/src/main/java/com/mera/babycare/Login.kt
@@ -15,34 +15,8 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val layoutEmail = findViewById<TextInputLayout>(R.id.layout_email)
-
-        val icon = ContextCompat.getDrawable(this, R.drawable.mail_icon)
-        if (icon != null) {
-            val resizedIcon = resizeDrawable(icon, 20, 20)
-            layoutEmail.setStartIconDrawable(resizedIcon)
-        }
-
-        val layoutPassword = findViewById<TextInputLayout>(R.id.layout_password)
-
-        val icon_password = ContextCompat.getDrawable(this, R.drawable.password_icon)
-
-        if (icon_password != null) {
-            val resizedIcon = resizeDrawable(icon_password, 20, 20)
-            layoutPassword.setStartIconDrawable(resizedIcon)
-        }
-    }
-
-    fun resizeDrawable(drawable: Drawable, width: Int, height: Int): Drawable {
-        val bitmap = (drawable as BitmapDrawable).bitmap
-        val resizedBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
-        return BitmapDrawable(resources, resizedBitmap)
     }
 }
--- app/src/main/java/com/mera/babycare/MainActivity.kt
+++ app/src/main/java/com/mera/babycare/MainActivity.kt
@@ -26,12 +26,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val intent = Intent(
+            this@MainActivity,
+            Login::class.java
+        )
+        startActivity(intent) // Inicia a nova Activity
 
         val googleSignInButton = findViewById<Button>(R.id.googleSignInButton)
         googleSignInButton.setOnClickListener {
