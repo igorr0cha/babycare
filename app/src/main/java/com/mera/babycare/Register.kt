5951d22cf49c789548175d3bb841e8d2a228d43e
Anthony Passos dos Santos <anthony.passos@dinfor.uniceub>
2025-06-18 16:50:28 -0300

    tela de cadastro

---
+++ app/src/main/java/com/mera/babycare/Register.kt
@@ -0,0 +1,14 @@
+package com.mera.babycare
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
